c9edfec4a469f6784c826f331d3c099a135827d4
StarOnline <kaadiryildiz@gmail.com>
2021-10-03 18:30:56 +0300

    Identity startup ayarlamaları yapıldı.

--- ShopAppV2.WebUI/Startup.cs
+++ ShopAppV2.WebUI/Startup.cs
@@ -9,10 +9,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using ShopAppV2.DataAccess.Abstract;
 using ShopAppV2.Business.Abstract;
 using ShopAppV2.Business.Concrete;
 using ShopAppV2.DataAccess.Concrete.EfCore;
+using ShopAppV2.WebUI.Identity;
 using ShopAppV2.WebUI.Middlewares;
 
 namespace ShopAppV2.WebUI
@@ -29,6 +32,52 @@ namespace ShopAppV2.WebUI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Identity kurulum start //
+            services.AddDbContext<ApplicationIdentityDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("IdentityConnection")));  //appsettings.json'da tanýmlandý.
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationIdentityDbContext>() //AddEntityFrameworkStores = datalar nerede saklansýn.
+                .AddDefaultTokenProviders(); //reset password gibi iþlemlerde benzersiz token üretmesi..
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                // password
+
+                options.Password.RequireDigit = true;                //sayýsal deðer ister
+                options.Password.RequireLowercase = true;            //küçük karakter ister
+                options.Password.RequiredLength = 6;                 // min 6 karakter olsun
+                options.Password.RequireNonAlphanumeric = true;     // Alfanumeric olmalý
+                options.Password.RequireUppercase = true;           // büyük harf olmalý
+
+                options.Lockout.MaxFailedAccessAttempts = 5;        // kaç kere yanlýþ girme hakký olsun
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);   //5 dakika boyunca engelle
+                options.Lockout.AllowedForNewUsers = true;                          //engelleme yeni kullanýcý için de geçerli olsun.
+
+                // options.User.AllowedUserNameCharacters = "";
+                options.User.RequireUniqueEmail = true;                             // ayný email'den yeni kullanýcý kayýt olamaz.
+
+                options.SignIn.RequireConfirmedEmail = true;                        // kullanýcýya email onay kodu gönderilsin.
+                options.SignIn.RequireConfirmedPhoneNumber = false;                 // kullanýcýya telefon ile onay gönderilsin
+            });
+
+
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.LoginPath = "/account/login";
+                options.LogoutPath = "/account/logout";
+                options.AccessDeniedPath = "/account/accessdenied";
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(60); // tarayýcýda saklanma süresi.
+                options.SlidingExpiration = true;
+                options.Cookie = new CookieBuilder
+                {
+                    HttpOnly = true,
+                    Name = ".ShopApp.Security.Cookie"
+                };
+
+            });
+            // Identity kurulum end //
+
             //services.AddScoped<IProductDal, MemoryProductDal>();
             services.AddScoped<IProductDal, EfCoreProductDal>();
             services.AddScoped<ICategoryDal, EfCoreCategoryDal>();
@@ -57,6 +106,7 @@ namespace ShopAppV2.WebUI
             {
                 app.UseExceptionHandler("/Error");
             }
+            app.UseAuthentication();  // Identity kullan..
             app.UseStaticFiles();
             app.CustomStaticFiles();  //Middlewares klasörü içerisine class eklendi node_modules için.
                                       //app.UseMvcWithDefaultRoute();
