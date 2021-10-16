6074996237fc747db6cf1b4f5716f654d394f4e9
kadiryildiz <kadiryildiz@mail.com>
2021-10-16 13:24:38 +0300

    Cart Entityleri oluşturuldu.

---
+++ ShopAppV2.Entities/Cart.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShopAppV2.Entities
+{
+    public class Cart
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+
+        public List<CartItem> CartItems { get; set; }  // cart içerisinde bulunan tüm cartItemler'ı alabilmek için.     Cart.CartItems 
+    }
+}
---
+++ ShopAppV2.Entities/CartItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShopAppV2.Entities
+{
+    public class CartItem
+    {
+        public int Id { get; set; }
+
+        public Product Product { get; set; }  //product tablosuna direk erişim için..
+        public int ProductId { get; set; }  //kullanıcı hangi ürünü ekledi ?
+
+        public Cart Cart { get; set; }
+        public int CartId { get; set; }     //kullanıcı hangi kartı eklemiş ?  cart.id = cartItem.CartId
+
+        public int Quantity { get; set; }
+    }
+}
