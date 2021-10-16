eead11f059534b95a56ba78bf32e234269516013
kadiryildiz <kadiryildiz@mail.com>
2021-10-16 13:49:53 +0300

    CartManager ve ICartDal implement edildi.

---
+++ ShopAppV2.Business/Concrete/CartManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ShopAppV2.Business.Abstract;
+using ShopAppV2.DataAccess.Abstract;
+using ShopAppV2.Entities;
+
+namespace ShopAppV2.Business.Concrete
+{
+    public class CartManager:ICartService
+    {
+        private ICartDal _cartDal;
+        public CartManager(ICartDal cartDal)
+        {
+            _cartDal = cartDal;
+        }
+        public void InitializeCart(string userId)
+        {
+            _cartDal.Create(new Cart() { UserId = userId });
+        }
+    }
+}
---
+++ ShopAppV2.DataAccess/Abstract/ICartDal.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ShopAppV2.Entities;
+
+namespace ShopAppV2.DataAccess.Abstract
+{
+    public interface ICartDal : IRepository<Cart>
+    {
+    }
+}
