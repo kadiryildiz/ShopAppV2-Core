cc25cda480544aebec80fb14360c2a99ce6807f4
kadiryildiz <kadiryildiz@mail.com>
2021-10-16 14:21:46 +0300

    Add to Cart css ayarlamaları yapıldı.

---
+++ ShopAppV2.WebUI/Controllers/CartController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+
+namespace ShopAppV2.WebUI.Controllers
+{
+    [Authorize]
+    public class CartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddToCart()
+        {
+            return View();
+        }
+    }
+}
